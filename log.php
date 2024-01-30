75ded7beeee8645223d11e0e8c6619d1e0bb8d8a
dtesters <144807230+dtesters@users.noreply.github.com>
2024-01-30 07:18:41 -0800

    Create log.php

---
+++ log.php
@@ -0,0 +1,11 @@
+
+$visitorJSON = file_get_contents('php://input');
+
+$visitor = json_decode($visitorJSON, true);
+
+$ip = $visitor['ip'];
+$browser = $visitor['browser'];
+
+$visitorData = date('Y-m-d H:i:s') . ' - ' . $ip . ' - ' . $browser . "\n";
+
+file_put_contents('visitors.txt', $visitorData, FILE_APPEND);
