67f4b9cfbafc2a28a0f65b70a57d42e9ccfc95b9
dtesters <144807230+dtesters@users.noreply.github.com>
2024-01-30 07:25:54 -0800

    Delete log.php

--- log.php
+++
@@ -1,8 +0,0 @@
-<?php
-$visitorJSON = file_get_contents('php://input');
-$visitor = json_decode($visitorJSON, true);
-$ip = $visitor['ip'];
-$browser = $visitor['browser'];
-$visitorData = date('Y-m-d H:i:s') . ' - ' . $ip . ' - ' . $browser . "\n";
-file_put_contents('visitors.txt', $visitorData, FILE_APPEND);
-?>
