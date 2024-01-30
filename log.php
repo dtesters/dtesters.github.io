7aa378612cf7e0fcdedf5bee7d276799c655ac03
dtesters <144807230+dtesters@users.noreply.github.com>
2024-01-30 07:19:28 -0800

    Update log.php

--- log.php
+++ log.php
@@ -1,11 +1,8 @@
-
+<?php
 $visitorJSON = file_get_contents('php://input');
-
 $visitor = json_decode($visitorJSON, true);
-
 $ip = $visitor['ip'];
 $browser = $visitor['browser'];
-
 $visitorData = date('Y-m-d H:i:s') . ' - ' . $ip . ' - ' . $browser . "\n";
-
 file_put_contents('visitors.txt', $visitorData, FILE_APPEND);
+?>
